4431e14b803cc218ac7dcbc86fc34c84933a37c6
Ernest Teluk <49727996+ErnestTeluk@users.noreply.github.com>
2024-01-22 08:30:04 +0100

    add base to playground config

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './sparrowql/',
     plugins: [react()],
 });
