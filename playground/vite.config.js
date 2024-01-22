51201ed8da5b33b5d183d39d9bcfe4ea64b2d4b1
Ernest Teluk <49727996+ErnestTeluk@users.noreply.github.com>
2024-01-22 08:36:37 +0100

    change base in playground config

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './sparrowql/',
+    base: '/sparrowql/',
     plugins: [react()],
 });
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './sparrowql/',
+  base: '/sparrowql/',
   plugins: [react()],
 })
