8ef0591f0d0c2f6585a1e77eebc36cacbbe53c45
Ernest Teluk <teluk.ernest@gmail.com>
2024-04-02 14:06:34 +0200

    fix google analytics script tag

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { vitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/sparrowql/',
-  plugins: [react(), vitePluginRadar({
-    enableDev: false,
-    gtm: {
-      id: 'G-ZDM0DBJJVX'
-    }
-  })],
+  plugins: [react()],
 })
